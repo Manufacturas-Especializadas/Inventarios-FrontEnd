1ab669ccb2328b35c449e93fae215a84854e0f41
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-23 07:20:26 -0600

    fix: sin placeholder

--- src/pages/Line12/L12EntryForm.tsx
+++ src/pages/Line12/L12EntryForm.tsx
@@ -229,7 +229,6 @@ export const L12EntryForm = () => {
                         onChange={(e) =>
                           handleChangeDetail(index, "client", e.target.value)
                         }
-                        placeholder="Opcional"
                         className="w-full px-3 py-1.5 border border-gray-300 rounded focus:ring-2 
                         focus:ring-blue-500 outline-none text-sm"
                       />
