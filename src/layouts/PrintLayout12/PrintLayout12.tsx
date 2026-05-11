36f489c79065b25f94854a08d9802c155470c6f3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-11 11:07:59 -0600

    fix(component): orientacion correcta

--- src/layouts/PrintLayout12/PrintLayout12.tsx
+++ src/layouts/PrintLayout12/PrintLayout12.tsx
@@ -10,8 +10,8 @@ const PrintBarcode = ({ value }: { value: string }) => {
       JsBarcode(svgRef.current, value, {
         format: "CODE128",
         lineColor: "#000",
-        width: 1.5,
-        height: 24,
+        width: 3,
+        height: 100,
         displayValue: false,
         margin: 0,
       });
@@ -19,38 +19,50 @@ const PrintBarcode = ({ value }: { value: string }) => {
   }, [value]);
 
   return (
-    <svg
-      ref={svgRef}
+    <div
       style={{
-        width: "32mm",
-        height: "8mm",
-        display: "block",
+        transform: "scale(3.2, 2.8)",
+        transformOrigin: "center",
+        overflow: "hidden",
+        height: "65mm",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
-    />
+    >
+      <svg
+        ref={svgRef}
+        style={{
+          width: "60mm",
+          height: "24mm",
+          display: "block",
+        }}
+      />
+    </div>
   );
 };
 
 export const PrintLayout12 = ({ foliosToPrint }: { foliosToPrint: any[] }) => {
   return (
     <div
-      className="hidden print:block print:absolute print:top-0 print:left-0
-      print:w-full print:bg-white print:z-9999 print:m-0 print:p-0"
+      className="hidden print:block print:absolute print:top-0 print:left-0 
+        print:w-full print:bg-white print:z-9999 print:m-0 print:p-0"
     >
       <style type="text/css" media="print">
         {`
-          @page {
-            size: landscape;
-            margin: 0;
-          }
+            @page { 
+              size: 400mm 300mm; 
+              margin: 0mm;
+            }
 
-          body {
-            margin: 0;
-            -webkit-print-color-adjust: exact;
-          }
-        `}
+            body { 
+              margin: 0; 
+              -webkit-print-color-adjust: exact;
+            }
+          `}
       </style>
 
-      {foliosToPrint.map((item, index) => {
+      {foliosToPrint.map((item: any, index) => {
         let rawFolio: any = item.folio || item;
 
         if (typeof rawFolio === "string" && rawFolio.startsWith("{")) {
@@ -70,30 +82,26 @@ export const PrintLayout12 = ({ foliosToPrint }: { foliosToPrint: any[] }) => {
         return (
           <div
             key={index}
-            className="relative mx-auto bg-white overflow-hidden"
+            className="relative mx-auto bg-white print:break-after-page overflow-hidden"
             style={{
-              width: "100vw",
-              height: "100vh",
-              padding: "5mm 10mm 2mm 10mm",
+              width: "400mm",
+              height: "250mm",
+              padding: "12mm 20mm",
               boxSizing: "border-box",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              overflow: "hidden",
-              gap: "2mm",
             }}
           >
             <div
               style={{
                 width: "100%",
                 display: "flex",
-                justifyContent: "flex-center",
+                justifyContent: "flex-end",
+                marginBottom: "8mm",
               }}
             >
               <div
                 style={{
-                  fontSize: "4mm",
-                  fontWeight: 700,
+                  fontSize: "20mm",
+                  fontWeight: 800,
                   color: "#000",
                 }}
               >
@@ -107,15 +115,15 @@ export const PrintLayout12 = ({ foliosToPrint }: { foliosToPrint: any[] }) => {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                flex: 1,
+                marginBottom: "10mm",
               }}
             >
               <div
                 style={{
-                  fontSize: "20mm",
+                  fontSize: "125mm",
                   fontWeight: 900,
-                  lineHeight: 1,
-                  letterSpacing: "-1mm",
+                  lineHeight: 0.9,
+                  letterSpacing: "-4mm",
                   color: "#000",
                 }}
               >
@@ -129,8 +137,9 @@ export const PrintLayout12 = ({ foliosToPrint }: { foliosToPrint: any[] }) => {
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "flex-end",
-                paddingLeft: "5mm",
-                paddingRight: "5mm",
+                paddingLeft: "25mm",
+                paddingRight: "25mm",
+                marginTop: "5mm",
               }}
             >
               <div
@@ -141,26 +150,15 @@ export const PrintLayout12 = ({ foliosToPrint }: { foliosToPrint: any[] }) => {
                 }}
               >
                 <PrintBarcode value={String(folioString)} />
-
-                {/* <div
-                  style={{
-                    fontSize: "3.5mm",
-                    fontWeight: 700,
-                    marginTop: "0.5mm",
-                    letterSpacing: "0.5mm",
-                  }}
-                >
-                  {folioText}
-                </div> */}
               </div>
 
               <img
                 src={Logo}
                 alt="logo"
                 style={{
-                  width: "18mm",
+                  width: "70mm",
                   objectFit: "contain",
-                  marginBottom: "1mm",
+                  marginBottom: "10mm",
                 }}
               />
             </div>
