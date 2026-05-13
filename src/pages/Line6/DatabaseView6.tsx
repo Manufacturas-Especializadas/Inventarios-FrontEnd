31d5a4ba2ef75f55e2ca0351ea6debcb1444e8e7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-13 11:16:17 -0600

    feat(component): base de datos L6

---
+++ src/pages/Line6/DatabaseView6.tsx
@@ -0,0 +1,12 @@
+import { GenericDatabaseView } from "../../components/UI/GenericDatabaseView";
+
+export const DatabaseView6 = () => {
+  return (
+    <GenericDatabaseView
+      lineId={5}
+      lineName="LÍNEA 6"
+      entryUrl="/entradas-linea-6"
+      exitUrl="/salidas-l6"
+    />
+  );
+};
