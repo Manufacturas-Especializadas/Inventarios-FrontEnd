2d32834244a20ff0b09494b357a92b8800dab413
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-27 08:12:28 -0600

    feat(component): componente de salidas para L4

---
+++ src/pages/Line4/ExitForm4.tsx
@@ -0,0 +1,12 @@
+import { GenericExitForm } from "../../components/UI/GenericExitForm";
+
+export const ExitForm4 = () => {
+  return (
+    <GenericExitForm
+      lineId={4}
+      lineName="LÍNEA 4"
+      entryUrl="/entradas-linea-4"
+      dbUrl="/base-de-datos-l4"
+    />
+  );
+};
