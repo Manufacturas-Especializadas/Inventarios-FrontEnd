d0b21b0932d37181b83e946da1eb1cb6594ea06c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-09 10:35:28 -0600

    feat(hook): hook microchannel

---
+++ src/hooks/usoMicroChannel.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { microChannelService } from "../api/services/MicroChannelService";
+
+export const useMicroChannel = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const registerScan = async (
+    code: string,
+    typeMovement: "ENTRADA" | "SALIDA",
+  ) => {
+    setIsSubmitting(true);
+
+    try {
+      await microChannelService.register({
+        code,
+        typeMovement,
+      });
+
+      return { success: true };
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.message ||
+        error.response?.data?.Message ||
+        error.message ||
+        "Error de conexión con la base de datos.";
+
+      console.error("Detalle del error:", errorMessage);
+
+      return { success: false, errorMessage: `${code}: ${errorMessage}` };
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return { registerScan, isSubmitting };
+};
