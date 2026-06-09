bd333d93adbf79e066858d5a8d5f83d94fc3a95b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-09 07:55:54 -0600

    feat(service): metodo para el registro

--- src/api/services/MicroChannelService.ts
+++ src/api/services/MicroChannelService.ts
@@ -1,3 +1,13 @@
-class MicroChannelService {}
+import { API_CONFIG } from "../../config/api";
+import type { MicroChannel } from "../../types/Types";
+import { apiClient } from "../client";
+
+class MicroChannelService {
+  private registerEndpoint = API_CONFIG.endpoints.microchannel.register;
+
+  async register(data: MicroChannel): Promise<void> {
+    return apiClient.post<void>(this.registerEndpoint, data);
+  }
+}
 
 export const microChannelService = new MicroChannelService();
