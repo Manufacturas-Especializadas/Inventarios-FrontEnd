8bab829de4c0ad2e62eb5b767d585798ad9188cc
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-06-09 10:34:53 -0600

    feat(service): metodo para obtener la lista de microchannel

--- src/api/services/MicroChannelService.ts
+++ src/api/services/MicroChannelService.ts
@@ -1,9 +1,14 @@
 import { API_CONFIG } from "../../config/api";
-import type { MicroChannel } from "../../types/Types";
+import type { MicroChannel, MicroChannelList } from "../../types/Types";
 import { apiClient } from "../client";
 
 class MicroChannelService {
+  private getListEndpoint = API_CONFIG.endpoints.microchannel.recent;
   private registerEndpoint = API_CONFIG.endpoints.microchannel.register;
+
+  async getList(): Promise<MicroChannelList[]> {
+    return apiClient.get<MicroChannelList[]>(this.getListEndpoint);
+  }
 
   async register(data: MicroChannel): Promise<void> {
     return apiClient.post<void>(this.registerEndpoint, data);
