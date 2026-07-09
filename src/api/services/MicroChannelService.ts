f291386ebd85b990c472d8792a1a7bcdaee30b15
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-09 11:13:22 -0600

    feat(service): metodo para desactivar un contenedor

--- src/api/services/MicroChannelService.ts
+++ src/api/services/MicroChannelService.ts
@@ -1,10 +1,15 @@
 import { API_CONFIG } from "../../config/api";
-import type { MicroChannel, MicroChannelList } from "../../types/Types";
+import type {
+  DesactivatePayload,
+  MicroChannel,
+  MicroChannelList,
+} from "../../types/Types";
 import { apiClient } from "../client";
 
 class MicroChannelService {
   private getListEndpoint = API_CONFIG.endpoints.microchannel.recent;
   private registerEndpoint = API_CONFIG.endpoints.microchannel.register;
+  private desactivateEndpoint = API_CONFIG.endpoints.microchannel.desactivate;
 
   async getList(): Promise<MicroChannelList[]> {
     return apiClient.get<MicroChannelList[]>(this.getListEndpoint);
@@ -12,6 +17,10 @@ class MicroChannelService {
 
   async register(data: MicroChannel): Promise<void> {
     return apiClient.post<void>(this.registerEndpoint, data);
+  }
+
+  async desactivate(data: DesactivatePayload): Promise<void> {
+    return apiClient.post<void>(this.desactivateEndpoint, data);
   }
 }
 
